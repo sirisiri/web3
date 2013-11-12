d0e79dbdc488950b7afea02187a09303be92ad9a
Napangsiri Wanpen <napangsiri@gmail.com>
2013-11-12 12:56:36 -0500

    07

---
+++ 07/index.php
@@ -0,0 +1,191 @@
+
+<?php 
+$capsOff = array("a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z");
+$capsOn = array("A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z");
+
+if(isset($_POST['letter'])) {
+        //CAPS OFF LETTERS HERE
+        if($_POST['letter'] == $capsOff[0]) {
+                $result = $_POST['letter'];
+        }
+        else if($_POST['letter'] == $capsOff[1]) {
+                $result = $_POST['letter'];
+        }
+        else if($_POST['letter'] == $capsOff[2]) {
+                $result = $_POST['letter'];
+        }
+        else if($_POST['letter'] == $capsOff[3]) {
+                $result = $_POST['letter'];
+        }
+        else if($_POST['letter'] == $capsOff[4]) {
+                $result = $_POST['letter'];
+        }
+        else if($_POST['letter'] == $capsOff[5]) {
+                $result = $_POST['letter'];
+        }
+        else if($_POST['letter'] == $capsOff[6]) {
+                $result = $_POST['letter'];
+        }
+        else if($_POST['letter'] == $capsOff[7]) {
+                $result = $_POST['letter'];
+        }
+        else if($_POST['letter'] == $capsOff[8]) {
+                $result = $_POST['letter'];
+        }
+        else if($_POST['letter'] == $capsOff[9]) {
+                $result = $_POST['letter'];
+        }
+        else if($_POST['letter'] == $capsOff[10]) {
+                $result = $_POST['letter'];
+        }
+        else if($_POST['letter'] == $capsOff[11]) {
+                $result = $_POST['letter'];
+        }
+        else if($_POST['letter'] == $capsOff[12]) {
+                $result = $_POST['letter'];
+        }
+        else if($_POST['letter'] == $capsOff[13]) {
+                $result = $_POST['letter'];
+        }
+        else if($_POST['letter'] == $capsOff[14]) {
+                $result = $_POST['letter'];
+        }
+        else if($_POST['letter'] == $capsOff[15]) {
+                $result = $_POST['letter'];
+        }
+        else if($_POST['letter'] == $capsOff[16]) {
+                $result = $_POST['letter'];
+        }
+        else if($_POST['letter'] == $capsOff[17]) {
+                $result = $_POST['letter'];
+        }
+        else if($_POST['letter'] == $capsOff[18]) {
+                $result = $_POST['letter'];
+        }
+        else if($_POST['letter'] == $capsOff[19]) {
+                $result = $_POST['letter'];
+        }
+        else if($_POST['letter'] == $capsOff[20]) {
+                $result = $_POST['letter'];
+        }
+        else if($_POST['letter'] == $capsOff[21]) {
+                $result = $_POST['letter'];
+        }
+        else if($_POST['letter'] == $capsOff[22]) {
+                $result = $_POST['letter'];
+        }
+        else if($_POST['letter'] == $capsOff[23]) {
+                $result = $_POST['letter'];
+        }
+        else if($_POST['letter'] == $capsOff[24]) {
+                $result = $_POST['letter'];
+        }
+        else if($_POST['letter'] == $capsOff[25]) {
+                $result = $_POST['letter'];
+        }
+        //CAPS ON LETTERS HERE
+        else if($_POST['letter'] == $capsOn[0]) {
+                $result = $_POST['letter'];
+        }
+        else if($_POST['letter'] == $capsOn[1]) {
+                $result = $_POST['letter'];
+        }
+        else if($_POST['letter'] == $capsOn[2]) {
+                $result = $_POST['letter'];
+        }
+        else if($_POST['letter'] == $capsOn[3]) {
+                $result = $_POST['letter'];
+        }
+        else if($_POST['letter'] == $capsOn[4]) {
+                $result = $_POST['letter'];
+        }
+        else if($_POST['letter'] == $capsOn[5]) {
+                $result = $_POST['letter'];
+        }
+        else if($_POST['letter'] == $capsOn[6]) {
+                $result = $_POST['letter'];
+        }
+        else if($_POST['letter'] == $capsOn[7]) {
+                $result = $_POST['letter'];
+        }
+        else if($_POST['letter'] == $capsOn[8]) {
+                $result = $_POST['letter'];
+        }
+        else if($_POST['letter'] == $capsOn[9]) {
+                $result = $_POST['letter'];
+        }
+        else if($_POST['letter'] == $capsOn[10]) {
+                $result = $_POST['letter'];
+        }
+        else if($_POST['letter'] == $capsOn[11]) {
+                $result = $_POST['letter'];
+        }
+        else if($_POST['letter'] == $capsOn[12]) {
+                $result = $_POST['letter'];
+        }
+        else if($_POST['letter'] == $capsOn[13]) {
+                $result = $_POST['letter'];
+        }
+        else if($_POST['letter'] == $capsOn[14]) {
+                $result = $_POST['letter'];
+        }
+        else if($_POST['letter'] == $capsOn[15]) {
+                $result = $_POST['letter'];
+        }
+        else if($_POST['letter'] == $capsOn[16]) {
+                $result = $_POST['letter'];
+        }
+        else if($_POST['letter'] == $capsOn[17]) {
+                $result = $_POST['letter'];
+        }
+        else if($_POST['letter'] == $capsOn[18]) {
+                $result = $_POST['letter'];
+        }
+        else if($_POST['letter'] == $capsOn[19]) {
+                $result = $_POST['letter'];
+        }
+        else if($_POST['letter'] == $capsOn[20]) {
+                $result = $_POST['letter'];
+        }
+        else if($_POST['letter'] == $capsOn[21]) {
+                $result = $_POST['letter'];
+        }
+        else if($_POST['letter'] == $capsOn[22]) {
+                $result = $_POST['letter'];
+        }
+        else if($_POST['letter'] == $capsOn[23]) {
+                $result = $_POST['letter'];
+        }
+        else if($_POST['letter'] == $capsOn[24]) {
+                $result = $_POST['letter'];
+        }
+        else if($_POST['letter'] == $capsOn[25]) {
+                $result = $_POST['letter'];
+        }
+        else {
+                $result = "STOP TRYING TO BREAK IN";
+        }
+}
+?> 
+
+<!doctype>
+<html>
+<head>
+        <title>LETTER WORM ATTACKS!</title>
+</head>
+
+<body>
+<h3>--------------------------------------------------<br>
+        |<br>
+        |  &nbsp;&nbsp;I'm <?php echo $result; ?> wormie. Wh<?php echo $result; ?><?php echo $result; ?><?php echo $result; ?><?php echo $result; ?><?php echo $result; ?><?php echo $result; ?>sh.<br>
+        ---\/---------------------------------------------<br>
+        <h1>('o')<?php echo $result; ?><?php echo $result; ?><?php echo $result; ?><?php echo $result; ?><?php echo $result; ?><?php echo $result; ?><?php echo $result; ?><?php echo $result; ?><?php echo $result; ?><?php echo $result; ?><?php echo $result; ?><?php echo $result; ?><?php echo $result; ?><?php echo $result; ?><?php echo $result; ?><?php echo $result; ?><?php echo $result; ?><?php echo $result; ?><?php echo $result; ?><?php echo $result; ?><?php echo $result; ?><?php echo $result; ?><?php echo $result; ?><?php echo $result; ?><?php echo $result; ?><?php echo $result; ?></h1>
+
+
+        <form method="post" action="index.php">
+                <input type="text" name="letter" placeholder="a-z or A-Z letter here" /><br />
+
+                <input type="submit" />
+        </form>
+</body>
+</html>
